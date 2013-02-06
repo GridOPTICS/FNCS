bf22a6620c354252e08505ad1cb9646422629c4a
cira582@PNL.GOV <cira582@PNL.GOV@fe075245-f7d2-0410-b612-98e2882219ba>
2013-02-06 19:46:00 +0000

    commit interface checking

---
+++ abscomminterface.cpp
@@ -0,0 +1,102 @@
+/*
+    Copyright (c) 2013, <copyright holder> <email>
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+        * Redistributions of source code must retain the above copyright
+        notice, this list of conditions and the following disclaimer.
+        * Redistributions in binary form must reproduce the above copyright
+        notice, this list of conditions and the following disclaimer in the
+        documentation and/or other materials provided with the distribution.
+        * Neither the name of the <organization> nor the
+        names of its contributors may be used to endorse or promote products
+        derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY <copyright holder> <email> ''AS IS'' AND ANY
+    EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL <copyright holder> <email> BE LIABLE FOR ANY
+    DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+    ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+
+#include "abscomminterface.h"
+#include "absintegrator.h"
+
+namespace sim_comm{
+AbsCommInterface::AbsCommInterface(uint32_t myRank)
+{
+  this->receiveCount=0;
+  this->sendCount=0;
+  this->receiverRunning=false;
+  this->myRank=myRank;
+}
+
+bool AbsCommInterface::isReceiverRunning()
+{
+  return this->receiverRunning;
+}
+
+void AbsCommInterface::addObjectInterface(string objectName, ObjectCommInterface* given)
+{
+  if(given->getMyRank()==myRank)
+    throw ObjectInterfaceRegistrationException();
+  
+  this->interfaces.insert(pair<string,ObjectCommInterface*>(objectName,given));
+}
+
+void AbsCommInterface::startReceiver()
+{
+ //implement
+}
+
+void AbsCommInterface::stopReceiver()
+{
+  this->receiverRunning=false;
+}
+
+void AbsCommInterface::sendAll()
+{
+  map<string,ObjectCommInterface*>::iterator it=this->interfaces.begin();
+  
+  for(;it!=this->interfaces.end();it++){
+  
+      ObjectCommInterface *in=it->second;
+      if(in->getInboxMessagesCount()>0){
+      
+	vector<AbsMessage*>  outmessges=in->getOutBox();
+	for(int i=0;i<outmessges.size();i++){
+	  try{
+	    this->realSendMessage(outmessges[i]);
+	    if(outmessges[i]->isBroadCast()){
+	    
+	      sendCount+=AbsIntegrator::getNumberOfCommNodes();
+	    }
+	    else{
+	    
+	      sendCount+=1;
+	    }
+	  }
+	  catch(InterfaceErrorException e){
+	  
+	    std::cerr << "Send operation failed on interface ";
+	  }
+	}
+      }
+  }
+}
+
+AbsCommInterface::~AbsCommInterface()
+{
+  this->interfaces.clear();
+}
+
+
+
+}
---
+++ abscomminterface.h
@@ -0,0 +1,111 @@
+/*
+    Copyright (c) 2013, <copyright holder> <email>
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+        * Redistributions of source code must retain the above copyright
+        notice, this list of conditions and the following disclaimer.
+        * Redistributions in binary form must reproduce the above copyright
+        notice, this list of conditions and the following disclaimer in the
+        documentation and/or other materials provided with the distribution.
+        * Neither the name of the <organization> nor the
+        names of its contributors may be used to endorse or promote products
+        derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY <copyright holder> <email> ''AS IS'' AND ANY
+    EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL <copyright holder> <email> BE LIABLE FOR ANY
+    DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+    ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+
+#ifndef ABSCOMMINTERFACE_H
+#define ABSCOMMINTERFACE_H
+
+#include "absmessage.h"
+#include <map>
+#include <pthread.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include "objectcomminterface.h"
+
+using namespace std;
+namespace sim_comm{
+ // class ObjectCommInterface;
+  class ObjectInterfaceRegistrationException : exception{
+  virtual const char* what() const throw()
+    {
+      return "Cannot attach interface to a non-remote object!";
+    }
+    
+    
+  };
+  
+    class InterfaceErrorException : exception{
+  virtual const char* what() const throw()
+    {
+      return "Interface operation failed!!";
+    }
+    
+    
+  };
+class AbsCommInterface
+{
+private:
+  map<string,ObjectCommInterface*> interfaces;
+  bool receiverRunning;
+  uint64_t sendCount,receiveCount;
+  uint32_t myRank;
+public:
+  /* The real send message method, calling this will send the message to another sim_comm
+   * Throws InterfaceErrorException if the send operation fails;
+   */
+  virtual void realSendMessage(AbsMessage *given) =0;
+  /* Reall receive message method, calling this will block until a message is received from a sim_comm
+   * Throw InterfaceErrorException if the receive operation fails
+   */
+  virtual AbsMessage* realGetMessage() =0;
+  /* Reduce min time operation.
+   * Throws InterfaceErrorException when the reduce operation fails
+   */
+  virtual uint64_t realReduceMinTime(uint64_t myTime) =0;
+  /* Reduce total send receive operation.
+   * Throws InterfaceErrorException when the reduce operation fails
+   */
+  virtual uint64_t realReduceTotalSendReceive(uint64_t send,uint64_t receive) =0;
+  AbsCommInterface(uint32_t myRank);
+  /*USed by the integrator to register an object.
+   */
+  void addObjectInterface(string objectName,ObjectCommInterface *given);
+  /* Starts the receiver thread.
+   */
+  void startReceiver();
+  /* Returns true if the reciever thread is running.
+   */
+  bool isReceiverRunning();
+  /* Kills the receiver thread
+   */
+  void stopReceiver();
+  /* Called by the integrator to send all the messages.
+   */
+  void sendAll();
+  /* Returns the rank of the simulator.
+   */
+  uint32_t getMyRank(){
+  
+     return this->myRank;
+  }
+  virtual ~AbsCommInterface();
+};
+
+}
+
+#endif // ABSCOMMINTERFACE_H
