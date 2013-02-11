409fcb76dfff062650007a5f73b69910f7bea685
cira582@PNL.GOV <cira582@PNL.GOV@fe075245-f7d2-0410-b612-98e2882219ba>
2013-02-11 20:12:08 +0000

    Test for seriaization of message class

---
+++ tests/messageTest.cpp
@@ -0,0 +1,34 @@
+#include "../message.h"
+#include "../integrator.h"
+#include "../util/time.h"
+#include <string.h>
+
+using namespace sim_comm;
+
+int main(){
+
+  uint8_t data[100];
+  
+  bzero(data,100);
+  data[99]=50;
+  data[50]=1;
+  data[0]=99;
+  
+  Integrator::initIntegrator(NULL,SECONDS,70,5);
+  
+  Message *t=new Message("selim","selim",0,data,100,0);
+  uint8_t *serbuff;
+  uint32_t size;
+  t->serialize(serbuff,size);
+  Message *deser=new Message(serbuff,size);
+  
+  uint8_t *dataCopy=deser->getData();
+  
+  if(dataCopy[0]!=data[0])
+    throw "SERIALIZATION FAILED!";
+   if(dataCopy[99]!=data[99])
+    throw "SERIALIZATION FAILED!";
+    if(dataCopy[50]!=data[50])
+    throw "SERIALIZATION FAILED!";
+  return 0;
+}
